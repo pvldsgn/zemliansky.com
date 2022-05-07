06bbef8719037481e38360c0f77d4652c35b2e1d
Pavel Z <sampashass@gmail.com>
2022-05-07 19:44:37 +0700

    all working normaly

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,6 @@ function watcher() {
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch.images, images);
-    gulp.watch(path.watch.svgicons, svgSprite);
 }
 
 export { svgSpriteTask }
