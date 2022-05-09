20d10ac76be09da87aa7cee125a85d41336d9a2c
Pavel Z <sampashass@gmail.com>
2022-05-09 07:01:58 +0700

    +fix arrow +fix overflow

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,26 @@
 import * as flsFunctions from "./modules/functions.js";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 flsFunctions.isWebp();
+
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollSmoother);
+
+// document.getElementsByClassName('card-item')[0].style = "border-radius: 5em";
+
+// gsap.to(".card-bg", {
+//     scrollTrigger: {
+//         scrub: 2
+//     },
+//     y: 100
+// });
+
+// const smoother = ScrollSmoother.create({
+//     content: ".main",
+//     smooth: 3,
+//     effects: true
+// });
+
+// smoother.effects(".card-bg", { speed: "auto" });
 
